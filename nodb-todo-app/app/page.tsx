f21db93dec1f9908eb8d5e91a712a9c416f6c5d4
Smustus <rikardfors_91@hotmail.com>
2024-04-02 11:15:14 +0200

    added code
    fixed troubles with interpretation

--- nodb-todo-app/app/page.tsx
+++ nodb-todo-app/app/page.tsx
@@ -4,30 +4,26 @@ import type Todo from './types';
 
 async function Home() {
 
-  /* const response = await fetch('/api/todos');
+  const response = await fetch('http://localhost:3000/api/todos');
   console.log(response);
   const data: Todo[] = await response.json();
-  console.log(data); */
-
-  const response = await fetch('/api/todos');
-  console.log(response);
+  console.log(data);
   
-
-  /* const todos = data.map((obj, index) => {
-      return (
-        <article key={obj.id} className={`${index === 0 ? 'rounded-t' : todos.length - 1 !== index ? '' : 'rounded-b'} grid grid-cols-4 auto-rows-auto p-6 hover:bg-slate-200 hover:cursor-pointer`}>
-          <section className="col-span-1 pr-2">
-            <h2 >{obj.title}</h2>
-          </section>
-          <section className="col-span-2 px-4">
-            <p >{obj.details.join(', ')}</p>
-          </section>
-          <section className="col-span-1 pl-2 text-end">
-            <p className={`${obj.completed ? 'text-green-800' : 'text-red-900'}`}>{obj.completed ? 'completed' : 'uncompleted'}</p>
-          </section>
-        </article>
-      )
-    }) */
+  const todos = data.map((obj, index) => {
+    return (
+      <article key={obj.id} className={`${index === 0 ? 'rounded-t' : data.length - 1 !== index ? '' : 'rounded-b'} grid grid-cols-4 auto-rows-auto p-6 hover:bg-slate-200 hover:cursor-pointer`}>
+        <section className="col-span-1 pr-2">
+          <h2 >{obj.title}</h2>
+        </section>
+        <section className="col-span-2 px-4">
+          <p >{obj.details.join(', ')}</p>
+        </section>
+        <section className="col-span-1 pl-2 text-end">
+          <p className={`${obj.completed ? 'text-green-800' : 'text-red-900'}`}>{obj.completed ? 'completed' : 'uncompleted'}</p>
+        </section>
+      </article>
+    )
+  })
   
   return (
     <main className="flex flex-col w-full min-h-screen justify-center items-center p-2 bg-slate-700 rounded">
@@ -38,7 +34,7 @@ async function Home() {
           <h3>Details</h3>
           <h3>Status</h3>
         </section>
-        {/* { todos } */}
+        { todos }
         
       </section>
     </main>
